a26085ea37ec6b56792375f7022a1e506da084ef
Ronit <sainironit6789@gmail.com>
2025-01-23 16:28:12 -0500

    Lab 1 completed last week !

--- labs/lab1/lab1-p2.js
+++ labs/lab1/lab1-p2.js
@@ -24,5 +24,9 @@ const app = http.createServer((req, res) => { // (() => {})
 let PORT = 8000;
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`)
-});
-    
+});  
+
+// note: I apologize for not using the HTML folder but 
+// I found the second part had worked when I had taken
+// the HTML files out of the HTML folder and instead 
+// bunched them with the .js files in Lab 1
